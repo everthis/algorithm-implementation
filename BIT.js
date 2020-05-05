854ff3451be7be0c05310f3350d6a513c0ca4d54
Nothing <wwwaap@gmail.com>
2020-05-05 22:29:24 +0800

    Update BIT.js

--- BIT.js
+++ BIT.js
@@ -2,15 +2,18 @@
 const lowBit = x => x & (-x)
 class FenwickTree {
   constructor(n) {
+    if(n < 1) return
     this.sum = Array(n + 1).fill(0)
   }
   update(i, delta) {
+    if(i < 1) return
     while(i < this.sum.length) {
       this.sum[i] += delta
       i += lowBit(i) 
     }
   }
   query(i) {
+    if(i < 1) return
     let sum = 0
     while(i > 0) {
       sum += this.sum[i]
