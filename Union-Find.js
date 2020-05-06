222dd80524af99093fcc209bfb027efea42257cf
Nothing <wwwaap@gmail.com>
2020-05-06 08:43:53 +0800

    Create Union-Find.js

---
+++ Union-Find.js
@@ -0,0 +1,19 @@
+class UnionFind {
+  constructor(n) {
+    this.parents = Array(n).fill(0).map((e, i) => i + 1)
+    this.ranks = Array(n).fill(0)
+  }
+  find(x) {
+    if(x !== this.parents[x]) this.parents[x] = this.find(this.parents[x])
+    return this.parents[x]
+  }
+  union(x, y) {
+    const [rx, ry] = [this.find(x), this.find(y)]
+    if(this.ranks[rx] > this.ranks[ry]) this.parents[ry] = rx
+    if(this.ranks[ry] > this.ranks[rx]) this.parents[rx] = ry
+    if(this.ranks[rx] === this.ranks[ry]) {
+      this.parents[ry] = rx
+      this.ranks[rx]++
+    }
+  }
+}
